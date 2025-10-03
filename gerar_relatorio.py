dca60d75606ed205de7d9dc484accc6146bafe4e
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-10-04 03:02:14 +0500

    update

--- gerar_relatorio.py
+++ gerar_relatorio.py
@@ -112,13 +112,6 @@ if __name__ == '__main__':
 if __name__ == '__main__':
     main(**CONFIG)
 
-if __name__ == '__main__':
-    main(**CONFIG)
-
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
 
 
 
