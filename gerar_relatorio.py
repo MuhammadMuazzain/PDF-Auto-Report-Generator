e1fa4ba221a300aaa506b93b9cb0dab2174d81a5
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-10-16 02:06:04 +0500

    update

--- gerar_relatorio.py
+++ gerar_relatorio.py
@@ -152,47 +152,6 @@ if __name__ == '__main__':
 if __name__ == '__main__':
     main(**CONFIG)
 
-if __name__ == '__main__':
-    main(**CONFIG)
-
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-
-if __name__ == '__main__':
-    main(**CONFIG)
 
 if __name__ == '__main__':
     main(**CONFIG)
@@ -202,27 +161,7 @@ if __name__ == '__main__':
     main(**CONFIG)
 
 
-if __name__ == '__main__':
-    main(**CONFIG)
 
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-
-if __name__ == '__main__':
-    main(**CONFIG)
-
-
-if __name__ == '__main__':
-    main(**CONFIG)
 
 
 if __name__ == '__main__':
